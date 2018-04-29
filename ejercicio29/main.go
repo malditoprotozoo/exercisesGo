9ce29872fdd47f9a177ce2e58d1b46d072c24192
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-04-29 17:54:05 -0300

    correcting exercise

--- ejercicio29/main.go
+++ ejercicio29/main.go
@@ -2,27 +2,20 @@
 reproducirse y lo hacen cada mes, cada pareja de conejos da a luz 2 conejos, estos
 extraños conejos son inmortales. Cree un programa que retorne el número de conejos
 existentes en una cantidad n de meses sabiendo que se inicia con una pareja de conejos
-recién nacidos.*/
+recién nacidos. */
 
 package main
 
 import "fmt"
 
-func rabbits(n int) int {
-	total := 0
-	for i := 2; i < n; i++ {
-		total++
+func rabbits(months int) int {
+	if months == 1 || months == 2 {
+		return 2
 	}
-	return total
+	return rabbits(months-1) + rabbits(months-2)
 }
 
 func main() {
-	months := 15
-	total := 0
-	for i := 0; i < months; i++ {
-		if i%2 == 0 {
-			total += rabbits(months)
-		}
-	}
-	fmt.Println(total)
+	months := 5
+	fmt.Println(rabbits(months))
 }
