927d62bf531842f8a68f794f98151535d98f1a60
malditoprotozoo <virodriguez.fe@gmail.com>
2018-04-30 17:23:47 -0300

    correcting exercise

--- ejercicio34/main.go
+++ ejercicio34/main.go
@@ -10,13 +10,11 @@ func main() {
 		{2, 3, 4, 6},
 		{0, 1, 1, 3},
 		{5, 1, 9, 0},
-		{4, 21, 2, 3}}
+		{4, 21, 2, 2}}
 
 	result := 0
-	index := 0
 	for i := 0; i < len(matrix); i++ {
-		result += matrix[i][index]
-		index++
+		result += matrix[i][i]
 	}
 	fmt.Println(result)
 }
