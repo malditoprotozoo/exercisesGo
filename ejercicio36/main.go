871d7efc619b33a19bd1a5c1d33f4f1bd147c3b8
malditoprotozoo <virodriguez.fe@gmail.com>
2018-05-02 14:47:19 -0300

    correcting exercise, replacing append with make

--- ejercicio36/main.go
+++ ejercicio36/main.go
@@ -1,5 +1,5 @@
-/*36. Escribir un programa que copie los elementos de los bordes en sentido horario de una
-matriz de nxm en un vector resultante llamado v, imprima su contenido. */
+/*36. Escribnxir un programa que copie los elementos de los bordes en sentido horario de una
+matriz de m en un vector resultante llamado v, imprima su contenido. */
 
 package main
 
@@ -8,28 +8,32 @@ import "fmt"
 func main() {
 	matrix := [][]int{
 		{2, 3, 4, 6},
-		{0, 1, 1, 3},
+		{1000, 1, 1, 3},
 		{5, 3, 9, 0},
 		{4, 21, 2, 3},
 		{99, 11, 22, 33},
 	}
-	v := []int{}
-	firstArr, rowsLen, lastArr := 0, len(matrix[0])-1, len(matrix)-1
+	v := make([]int, (len(matrix)*2)+(len(matrix[0])*2)-4)
+	counter := 0
 	// RIGHT
-	for i := 0; i < len(matrix[firstArr]); i++ {
-		v = append(v, matrix[firstArr][i])
+	for i := 0; i < len(matrix[0]); i++ {
+		counter++
+		v[i] = matrix[0][i]
 	}
 	// DOWN
 	for i := 1; i < len(matrix); i++ {
-		v = append(v, matrix[i][rowsLen])
+		counter++
+		v[len(matrix[0])+i-1] = matrix[i][len(matrix[0])-1]
 	}
 	// LEFT
-	for i := len(matrix[firstArr]) - 2; i >= 0; i-- {
-		v = append(v, matrix[lastArr][i])
+	for i := len(matrix[0]) - 2; i >= 0; i-- {
+		counter++
+		v[len(v)-len(matrix[0])-i] = matrix[len(matrix)-1][i]
 	}
 	// TOP
-	for i := lastArr - 1; i >= 0; i-- {
-		v = append(v, matrix[i][0])
+	for i := len(matrix) - 2; i >= 1; i-- {
+		v[counter] = matrix[i][0]
+		counter++
 	}
 	fmt.Println(v)
 }
