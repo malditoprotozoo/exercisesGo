ae3da3b4fa47ededf0d772563f1f736c2bc09a54
malditoprotozoo <virodriguez.fe@gmail.com>
2018-05-02 14:55:08 -0300

    correcting exercise with a recursive function

--- ejercicio45/main.go
+++ ejercicio45/main.go
@@ -5,24 +5,35 @@ meses. Las ranas se reproducen cada tres meses. Cada 6 meses, en los días poste
 la reproducción, se realiza una comilona caníbal donde cada una de las ranas
 reproductivas se come a 20 parejas de ranas no reproductivas. Realice un programa que
 dado un número de meses ingresado por el usuario y sabiendo que se inicia con una
-pareja de ranas, entregue el número de ranas al finalizar el periodo de tiempo señalado. */
+pareja de ranas, entregue el número de ranas al finalizar el periodo de tiempo señalado.
+
+CORREGIR FUNCIÓN RECURSIVA*/
 
 package main
 
 import "fmt"
 
-func frogs(months int) int {
-	total := 0
-	if months >= 0 && months <= 2 {
+/*func rabbits(months int) int {
+	if months == 1 || months == 2 {
 		return 2
 	}
-	if months%3 == 0 {
-		total += (months / 3) * 100
+	return rabbits(months-1) + rabbits(months-2)
+}
+
+func main() {
+	months := 5
+	fmt.Println(rabbits(months))
+}*/
+
+func frogs(months int) int {
+	bornedFrogs := 100
+	if months == 1 || months == 2 {
+		return 2
 	}
 	if months%6 == 0 {
-		total -= (months / 6) * 20
+		bornedFrogs -= 60
 	}
-	return total
+	return (frogs(months-1) + frogs(months-2)) + bornedFrogs
 }
 
 func main() {
